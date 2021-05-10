73c518eb3a7f19d88243ecc569757a9f5c13d784
Duc Nguyen <minhduc140583@gmail.com>
2021-05-09 22:09:49 -1200

    Add pubsub + firestore

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"reflect"
 
+	"github.com/core-go/firestore"
 	"github.com/core-go/health"
-	"github.com/core-go/mongo"
 	"github.com/core-go/mq"
 	"github.com/core-go/mq/log"
 	"github.com/core-go/mq/pubsub"
@@ -22,9 +22,8 @@ type ApplicationContext struct {
 
 func NewApp(ctx context.Context, root Root) (*ApplicationContext, error) {
 	log.Initialize(root.Log)
-	db, er1 := mongo.SetupMongo(ctx, root.Mongo)
+	client, er1 := firestore.Connect(ctx, []byte(root.Firestore.Credentials))
 	if er1 != nil {
-		log.Error(ctx, "Cannot connect to MongoDB. Error: "+er1.Error())
 		return nil, er1
 	}
 
@@ -41,10 +40,10 @@ func NewApp(ctx context.Context, root Root) (*ApplicationContext, error) {
 	}
 
 	userType := reflect.TypeOf(User{})
-	batchWriter := mongo.NewBatchInserter(db, "user")
+	batchWriter := firestore.NewBatchInserter(client, "user", userType)
 	batchHandler := mq.NewBatchHandler(userType, batchWriter.Write, logError, logInfo)
 
-	mongoChecker := mongo.NewHealthChecker(db)
+	firestoreChecker := firestore.NewHealthChecker(ctx, []byte(root.Firestore.Credentials))
 	receiverChecker := pubsub.NewSubHealthChecker("pubsub_subscriber", receiver.Client, root.Sub.SubscriptionId)
 	var healthHandler *health.HealthHandler
 	var batchWorker mq.BatchWorker
@@ -58,10 +57,10 @@ func NewApp(ctx context.Context, root Root) (*ApplicationContext, error) {
 		retryService := mq.NewRetryService(sender.Publish, logError, logInfo)
 		batchWorker = mq.NewDefaultBatchWorker(root.BatchWorkerConfig, batchHandler.Handle, retryService.Retry, logError, logInfo)
 		senderChecker := pubsub.NewPubHealthChecker("pubsub_publisher", sender.Client, root.Pub.TopicId)
-		healthHandler = health.NewHealthHandler(mongoChecker, receiverChecker, senderChecker)
+		healthHandler = health.NewHealthHandler(firestoreChecker, receiverChecker, senderChecker)
 	} else {
 		batchWorker = mq.NewDefaultBatchWorker(root.BatchWorkerConfig, batchHandler.Handle, nil, logError, logInfo)
-		healthHandler = health.NewHealthHandler(mongoChecker, receiverChecker)
+		healthHandler = health.NewHealthHandler(firestoreChecker, receiverChecker)
 	}
 	checker := validator.NewErrorChecker(NewUserValidator().Validate)
 	validator := mq.NewValidator(userType, checker.Check)
--- internal/app/root.go
+++ internal/app/root.go
@@ -1,8 +1,8 @@
 package app
 
 import (
+	"github.com/core-go/firestore"
 	"github.com/core-go/health/server"
-	"github.com/core-go/mongo"
 	"github.com/core-go/mq"
 	"github.com/core-go/mq/log"
 	"github.com/core-go/mq/pubsub"
@@ -11,7 +11,7 @@ import (
 type Root struct {
 	Server            server.ServerConf       `mapstructure:"server"`
 	Log               log.Config              `mapstructure:"log"`
-	Mongo             mongo.MongoConfig       `mapstructure:"mongo"`
+	Firestore         firestore.Config        `mapstructure:"firestore"`
 	BatchWorkerConfig mq.BatchWorkerConfig    `mapstructure:"batch_worker"`
 	Sub               pubsub.SubscriberConfig `mapstructure:"sub"`
 	Pub               *pubsub.PublisherConfig `mapstructure:"pub"`
